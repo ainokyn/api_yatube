13a7559bce6b4381a305a568914fbfc41b461d6a
Alexandra <aleksandra.alexand2017@yandex.ru>
2021-10-19 00:16:55 +0300

    19.10.2021_00:16

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -10,8 +10,6 @@ router.register('groups', GroupViewSet)
 router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
                 basename='comments')
 urlpatterns = [
-    path(' ', include(router.urls)),
-    path('v1/api-token-auth/', views.obtain_auth_token),
     path('v1/', include(router.urls)),
+    path('v1/api-token-auth/', views.obtain_auth_token),
 ]
-urlpatterns += router.urls
